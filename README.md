d2f8357218b3e24ee779cb6f89605de0c238a4f6
Jungjin Park <jin@Jungjins-MacBook-Pro.local>
2022-07-22 21:51:57 -0700

    implement direction map

--- README.md
+++ README.md
@@ -634,7 +634,64 @@
       ...
     ```
 
-# Tips
+## Implement Directions Map
+
+- `yarn add react-native-maps-directions`
+
+- On `/components/Map.js`
+
+  - ```js
+    import React, { useEffect, useRef } from 'react';
+    ...
+    import { selectDestination, selectOrigin } from '../slices/navSlice';
+    import MapViewDirections from 'react-native-maps-directions';
+    import { GOOGLE_API_KEY } from '@env';
+
+    const Map = () => {
+      ...
+      const destination = useSelector(selectDestination);
+      const mapRef = useRef(null);
+
+      useEffect(() => {
+        if (!origin || !destination) return;
+
+        // Zoom & Fit to Markers
+        mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
+          edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+        });
+      }, [origin, destination]);
+      return (
+        <MapView
+          ...
+        >
+          {origin && destination && (
+            <MapViewDirections
+              origin={origin.description}
+              destination={destination.description}
+              apikey={GOOGLE_API_KEY}
+            ></MapViewDirections>
+          )}
+          {origin?.location && (
+            ...
+          )}
+          {destination?.location && (
+            <Marker
+              coordinate={{
+                latitude: destination.location.lat,
+                longitude: destination.location.lng,
+              }}
+              title='Destination'
+              description={destination.description}
+              identifier='destination'
+            />
+          )}
+        </MapView>
+        ...
+    ```
+
+---
+
+## Tips
 
 - Shortcut `rnfes`: reactNativeFunctionalExportComponentWithStyle
 
--- components/Map.js
+++ components/Map.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import MapView, { Marker } from 'react-native-maps';
 import tw from 'tailwind-react-native-classnames';
 import { useSelector } from 'react-redux';
-import { selectOrigin } from '../slices/navSlice';
+import { selectDestination, selectOrigin } from '../slices/navSlice';
+import MapViewDirections from 'react-native-maps-directions';
+import { GOOGLE_API_KEY } from '@env';
 
 const Map = () => {
   const origin = useSelector(selectOrigin);
-  console.log(origin);
+  const destination = useSelector(selectDestination);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+
+    // Zoom & Fit to Markers
+    mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
+      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+    });
+  }, [origin, destination]);
   return (
     <MapView
+      ref={mapRef}
       style={tw`flex-1`}
       initialRegion={{
         latitude: origin?.location?.lat,
@@ -17,6 +30,13 @@ const Map = () => {
         longitudeDelta: 0.005,
       }}
     >
+      {origin && destination && (
+        <MapViewDirections
+          origin={origin.description}
+          destination={destination.description}
+          apikey={GOOGLE_API_KEY}
+        ></MapViewDirections>
+      )}
       {origin?.location && (
         <Marker
           coordinate={{
@@ -26,6 +46,17 @@ const Map = () => {
           title='Origin'
           description={origin.description}
           identifier='origin'
+        />
+      )}
+      {destination?.location && (
+        <Marker
+          coordinate={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
+          }}
+          title='Destination'
+          description={destination.description}
+          identifier='destination'
         />
       )}
     </MapView>
